4408012c34a0bd19766099af89b5745ed20117c7
Wesley Marques <wesleymqsss@gmail.com>
2025-04-25 23:58:34 -0300

    Teste e ajuste de rotas

--- pro-conect/src/app/app.module.ts
+++ pro-conect/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './pages/home/home.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HomeModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
