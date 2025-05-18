b9eeaf0e24aa1e779137b88a4a4660fbb948bb21
Wesley Marques <wesleymqsss@gmail.com>
2025-05-18 00:29:54 -0300

    feature/login - layout pagina de login - finalização

--- pro-conect/src/app/shared/form-login/form-login.component.ts
+++ pro-conect/src/app/shared/form-login/form-login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../core/service/login.service';
+import { UserLogin } from '../../core/interface/userLogin';
+import { MessageService } from 'primeng/api';
+import { SnackbarService } from '../../core/service/snackbar.service';
+
 
 @Component({
   selector: 'app-form-login',
@@ -7,6 +12,12 @@ import { Component } from '@angular/core';
   styleUrl: './form-login.component.scss'
 })
 export class FormLoginComponent {
+  userLogin!: UserLogin;
+
+  constructor(
+    private readonly _userLoginService: LoginService,
+    private readonly _snackbarService: SnackbarService) { }
+
   name: string = '';
 
   email: string = '';
@@ -17,9 +28,26 @@ export class FormLoginComponent {
 
   load() {
     this.loading = true;
-    
     setTimeout(() => {
+      if (this.accept !== false) {
+        this.getUserLogin();
+      } else {
+        console.log('sou o termo de falso', this.accept);
+        this._snackbarService.showWarn('Favor, aceitar termos de uso!');
+      }
+
       this.loading = false
     }, 2000);
+  }
+
+  getUserLogin() {
+      console.log('sou o termo de verdadeiro', this.accept);
+      this._userLoginService.getUserLogin(this.name, this.email).subscribe({
+        next: (responseUserLogin) => {
+          this.userLogin = responseUserLogin;
+        }, error: (err) => {
+          this._snackbarService.showError('Usuário não encontrado. Favor, verificar dados de login!');
+        }
+      })
   }
 }
