a9614f07fef4a36f388429ff6c6af1e9b91a735f
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-05-25 22:26:59 -0300

    feat: Passando os dados via @Input para os componente

--- pro-conect/src/app/pages/home/home.component.ts
+++ pro-conect/src/app/pages/home/home.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
 import { LoginService } from '../../core/service/login.service';
 import { UserLogin } from '../../core/interface/userLogin';
+import { AlunoService } from '../../core/service/aluno.service';
+import { AlunoListResponse } from '../../core/types/aluno-list-response.type';
+import { CardDashboardService } from '../../core/service/card-dashboard.service';
+import { CardDashboardListResponse } from '../../core/types/card-dashboard-list-response.type';
 
 @Component({
   selector: 'app-home',
@@ -12,14 +16,20 @@ import { UserLogin } from '../../core/interface/userLogin';
 export class HomeComponent {
   userId!: string;
   userLogin!: UserLogin;
+  alunos!: AlunoListResponse;
+  cards!: CardDashboardListResponse;
 
   constructor(
     private readonly _route: ActivatedRoute,
-    private readonly _loginService: LoginService
+    private readonly _loginService: LoginService,
+    private readonly _alunoService: AlunoService,
+    private readonly _cardDashboardService: CardDashboardService
   ) { }
 
   ngOnInit() {
-     this.idUser();
+    this.idUser();
+    this.getStudents();
+    this.getCardsDashboard();
   }
 
   idUser() {
@@ -27,14 +37,25 @@ export class HomeComponent {
     const userIdInt = parseInt(this.userId);
     this.getUser(userIdInt);
   }
-  
+
   getUser(id: number) {
     this._loginService.getUserId(id).subscribe({
       next: (responseUserLogin) => {
         this.userLogin = responseUserLogin;
-        console.log(this.userLogin);
       }
     });
+  }
+
+  getStudents() {
+    this._alunoService.getAlunos().subscribe((alunoResponse) => {
+      this.alunos = alunoResponse;
+    })
+  }
+
+  getCardsDashboard() {
+    this._cardDashboardService.getCardsDashboard().subscribe((cardsResponse) => {
+      this.cards = cardsResponse;
+    })
   }
 
 }
