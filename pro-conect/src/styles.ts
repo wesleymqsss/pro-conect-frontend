6bc1082fcdd4b77a04d82f46c310c19848917d13
Wesley Marques <wesleymqsss@gmail.com>
2025-05-25 22:11:56 -0300

    bug - ajuste de bug ao atualizar informacoes do usuario

--- pro-conect/src/styles.ts
+++ pro-conect/src/styles.ts
@@ -4,39 +4,39 @@ import  Aura  from '@primeng/themes/aura';
 export const Noir = definePreset(Aura, {
     semantic: {
         primary: {
-            50: '{rose.50}',
-            100: '{rose.100}',
-            200: '{rose.200}',
-            300: '{rose.300}',
-            400: '{rose.400}',
-            500: '{rose.500}',
-            600: '{rose.600}',
-            700: '{rose.700}',
-            800: '{rose.800}',
-            900: '{rose.900}',
-            950: '{rose.950}'
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.600}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
         },
         colorScheme: {
             light: {
                 primary: {
-                    color: '{rose.700}',
+                    color: '{indigo.700}',
                     inverseColor: '#ffffff',
-                    hoverColor: '{rose.900}',
-                    activeColor: '{rose.500}'
+                    hoverColor: '{indigo.900}',
+                    activeColor: '{indigo.500}'
                 },
                 highlight: {
-                    background: '{rose.950}',
-                    focusBackground: '{rose.700}',
+                    background: '{indigo.950}',
+                    focusBackground: '{indigo.700}',
                     color: '#ffffff',
                     focusColor: '#ffffff'
                 }
             },
             dark: {
                 primary: {
-                    color: '{rose.600}',
-                    inverseColor: '{rose.500}',
-                    hoverColor: '{rose.100}',
-                    activeColor: '{rose.100}'
+                    color: '{indigo.600}',
+                    inverseColor: '{indigo.500}',
+                    hoverColor: '{indigo.100}',
+                    activeColor: '{indigo.100}'
                 },
                 highlight: {
                     background: 'rgba(250, 250, 250, .16)',
