2da476d7c60b798efd2e0266081dce3a38b67490
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-06-05 19:19:23 -0300

    feat: Adiciona novas propriedades

--- pro-conect/src/app/core/interface/avaliacao.ts
+++ pro-conect/src/app/core/interface/avaliacao.ts
@@ -1,23 +1,25 @@
-export interface IOpcao{
-    rotulo: string;
-    descricao: string;
-    correta: boolean;
+export interface IOpcao {
+  id: number
+  rotulo: string;
+  descricao: string;
+  correta: boolean;
 }
 
-export interface IQuestao{
-    texto: string;
-    valor: string;
-    professorId: number;
-    opcoes: IOpcao[];
+export interface IQuestao {
+  id: number;
+  texto: string;
+  valor: string;
+  professorId: number;
+  opcoes: IOpcao[];
 }
 
-export interface IProvaP{
-    id?: number;
-    descricao: string;
-    dataProva: string;
-    materiaId:number;
-    professorId: number;
-    questoes: IQuestao[];
+export interface IProvaP {
+  id?: number;
+  descricao: string;
+  dataProva: string;
+  materiaId: number;
+  professorId: number;
+  questoes: IQuestao[];
 }
 
 export interface IProvaDetalhes {
